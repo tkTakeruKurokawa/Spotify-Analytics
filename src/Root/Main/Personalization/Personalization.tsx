4fecebc8071c8f5010b8061c1efbac79f202f434
tkTakeruKurokawa <kurokawa.takeru830@icloud.com>
2021-05-05 15:16:41 +0900

    added image to get top music

--- src/Root/Main/Personalization/Personalization.tsx
+++ src/Root/Main/Personalization/Personalization.tsx
@@ -2,16 +2,42 @@ import axios from "axios";
 import { useContext, useEffect, useState } from "react";
 import { TokenContext } from "../../App";
 
-const GetTopMusic = (type: string): Array<string> => {
+const getTopArtists = (): JSX.Element => {
+  const response = GetDataFromSpotifyAPI("artists");
+
+  return (
+    <div className="artists">
+      {response.map((items, index) => {
+        const image = items.images[1].url;
+        const artist = items.name;
+
+        return buildJSX("artist", index + 1, image, artist);
+      })}
+    </div>
+  );
+};
+
+const getTopTracks = (): JSX.Element => {
+  const response = GetDataFromSpotifyAPI("tracks");
+
+  return (
+    <div className="tracks">
+      {response.map((items, index) => {
+        const image = items.album.images[1].url;
+        const track = items.name;
+
+        return buildJSX("track", index + 1, image, track);
+      })}
+    </div>
+  );
+};
+
+const GetDataFromSpotifyAPI = (type: string): Array<any> => {
   const [response, setResponse] = useState([]);
   const token = useContext(TokenContext);
   const uri = "https://api.spotify.com/v1/me/top/" + type;
 
   useEffect(() => {
-    if (!token) {
-      return;
-    }
-
     axios
       .get(uri, {
         headers: {
@@ -21,19 +47,38 @@ const GetTopMusic = (type: string): Array<string> => {
         },
       })
       .then((response) => {
-        const topMusic = response.data.items.map((item: any) => item.name);
+        // const topMusic = response.data.items.map((item: any) => item.name);
 
-        setResponse(topMusic);
+        setResponse(response.data.items);
       })
       .catch((error) => console.log(error));
-  }, [token]);
+  }, []);
 
   return response;
 };
 
+const buildJSX = (
+  className: string,
+  index: number,
+  image: string,
+  data: string
+): JSX.Element => {
+  const keyImage = "image-" + index;
+  const keyData = className + "-" + index;
+
+  return (
+    <div className={className}>
+      <span>{index}</span>
+      <img key={keyImage} src={image} alt={data} width="200" height="200" />
+      <span key={keyData}>{data}</span>
+      <br />
+    </div>
+  );
+};
+
 const Personalization = () => {
-  const topArtists = GetTopMusic("artists");
-  const topTracks = GetTopMusic("tracks");
+  const topArtists = getTopArtists();
+  const topTracks = getTopTracks();
 
   return (
     <div>
