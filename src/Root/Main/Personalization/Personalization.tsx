344c00e09433fd760f0f4aa363c759a5d690248b
tkTakeruKurokawa <kurokawa.takeru830@icloud.com>
2021-05-08 13:37:54 +0900

    added redirect to login when access token expired

--- src/Root/Main/Personalization/Personalization.tsx
+++ src/Root/Main/Personalization/Personalization.tsx
@@ -1,15 +1,28 @@
 import axios from "axios";
 import { useContext, useEffect, useState } from "react";
+import { Redirect, Router } from "react-router-dom";
 import { TokenContext } from "../../App";
 
 const getTopArtists = (): JSX.Element => {
   const response = GetDataFromSpotifyAPI("artists");
+
+  if (response[0] === "error") {
+    return (
+      <div>
+        <Redirect to="/login" />
+      </div>
+    );
+  }
 
   return (
     <div className="artists">
       {response.map((items, index) => {
         const image = items.images[1].url;
         const artist = items.name;
+
+        if (index === 2) {
+          console.log("Artists", items);
+        }
 
         return buildJSX("artist", index + 1, image, artist);
       })}
@@ -20,11 +33,23 @@ const getTopArtists = (): JSX.Element => {
 const getTopTracks = (): JSX.Element => {
   const response = GetDataFromSpotifyAPI("tracks");
 
+  if (response[0] === "error") {
+    return (
+      <div>
+        <Redirect to="/login" />
+      </div>
+    );
+  }
+
   return (
     <div className="tracks">
       {response.map((items, index) => {
         const image = items.album.images[1].url;
         const track = items.name;
+
+        if (index === 0) {
+          console.log("Tracks", items);
+        }
 
         return buildJSX("track", index + 1, image, track);
       })}
@@ -33,7 +58,7 @@ const getTopTracks = (): JSX.Element => {
 };
 
 const GetDataFromSpotifyAPI = (type: string): Array<any> => {
-  const [response, setResponse] = useState([]);
+  const [response, setResponse] = useState<any[]>([]);
   const token = useContext(TokenContext);
   const uri = "https://api.spotify.com/v1/me/top/" + type;
 
@@ -51,7 +76,10 @@ const GetDataFromSpotifyAPI = (type: string): Array<any> => {
 
         setResponse(response.data.items);
       })
-      .catch((error) => console.log(error));
+      .catch((error) => {
+        setResponse(["error"]);
+        console.log(error);
+      });
   }, []);
 
   return response;
